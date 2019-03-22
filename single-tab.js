0ba8412f6570215a96684c1a6bd804278cec8b2d
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-03-21 17:56:59 -0700

    [added] CSS to each box make it display as flex

--- single-tab.js
+++ single-tab.js
@@ -8,19 +8,23 @@ const something = chrome.tabs.query({currentWindow: true}, function (tabs) {
 
     const elArray = tabs.forEach((tab) => {
         const el = document.createElement('div')
-        el.style.width = '30%' // width: 100px
-        el.style.height = '100px'
-        el.style.backgroundColor = '#42f4b6'
-        el.style.margin = '2px'
+        // el.style.width = '30%' // width: 100px
+        // el.style.height = '100px'
+        // el.style.backgroundColor = '#42f4b6'
+        // el.style.margin = '2px'
         
         // ICON
+        const imgBox = document.createElement('div')
+        imgBox.style.borderRight = '2px solid #000'
+
         var img = document.createElement("img");
         img.src = tab.favIconUrl
-        img.style.height = "70px"
-        el.appendChild(img)
+        img.style.height = "40px"
+        imgBox.appendChild(img)
+        el.appendChild(imgBox)
 
         // TITLE
-        var tabTitle = document.createElement("h5")
+        var tabTitle = document.createElement("h3")
         tabTitle.innerHTML = tab.title
         el.appendChild(tabTitle)
 
