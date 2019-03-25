3725489fbd1820e12b0663d949ee25cbfb74f4ca
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-03-24 21:44:51 -0700

    [cleaned] the structure

--- background.js
+++ background.js
@@ -1,9 +1,16 @@
 // chrome.browserAction.setIcon({
 //     path: "stack - icon.png"
 // });
+// const numberOftabs = chrome.tabs.query({ currentWindow: true}, function (tabs) {
+//     return tabs.lenght
+// })
 chrome.browserAction.setBadgeBackgroundColor({
     color: [255, 0, 0, 255]
 });
-chrome.browserAction.setBadgeText({
-    text: numberOftabs
+// chrome.browserAction.setBadgeText({
+//     text: '+numberOftabs+'
+// });
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    chrome.browserAction.setBadgeText({"text": "hi"});
 });
